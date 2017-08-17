bea7e9db46b6acfad16f381321107f2d9709d539
System Administrator <root@sid.local>
2017-08-17 03:38:52 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuthModule  } from 'angularfire2/auth';
+import {  AngularFireAuth } from 'angularfire2/auth';
+import {  AngularFireModule } from 'angularfire2';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'SidChat';
+  //user: Observable<firebase.User>;
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+
+  constructor(public af: AngularFireAuth,public db: AngularFireDatabase) {
+    this.items= db.list('/messages', {query: {limitToLast: 5} });
+  this.af.authState.subscribe(auth => { if(auth) {this.name = auth; }});
+}
+
+
+
+
+login() {
+  this.af.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+}
+
+
+
+
+
+chatSend(theirMessage: string) {
+  this.items.push({ message: theirMessage, name:name});
+  this.msgVal = '';
+}
+
+
+logout() {
+  this.af.auth.signOut();
+}
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { HttpModule } from '@angular/http';
+
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAiP6Rn7h3ndcYM8qtNxzOWZbXrHu5E-p0",
+  authDomain: "sidchat-3babf.firebaseapp.com",
+  databaseURL: "https://sidchat-3babf.firebaseio.com",
+  projectId: "sidchat-3babf",
+  storageBucket: "sidchat-3babf.appspot.com",
+  messagingSenderId: "767308829588"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
